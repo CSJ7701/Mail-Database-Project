3b48006b5d2c6fe8bab516da327aeda1d4875bf3
Christian <c.ship.johnson@gmail.com>
2024-01-25 07:49:53 -0500

    Autocommit - 25Jan2024 07:49:50

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import utils
 from utils import *
 
-window=tk.Tk()
-window.geomtry=("400x450")
-window.title("Mail Database")
+root=tk.Tk()
+root.geomtry=("400x450")
+root.title("Mail Database")
 
 # Define tab system and create tabs
-tabControl=ttk.Notebook(window)
+tabControl=ttk.Notebook(root)
 tab1=ttk.Frame(tabControl)
 tab2=ttk.Frame(tabControl)
 tabControl.add(tab1, text='  Add Package  ')
@@ -21,6 +21,7 @@ package_box.grid(column=1, row=1, padx=30, pady=10)
 package_box_label=ttk.Label(tab1, text='Box Number').grid(row=1, column=0, padx=10, pady=10)
 package_button = ttk.Button(tab1, text="Get Info", command=lambda: get_cadet_info(package_box.get())).grid(row=2, column=1, pady=5)
 
-get_cadet_info(101)
 
-window.mainloop()
+root.mainloop()
+
+
