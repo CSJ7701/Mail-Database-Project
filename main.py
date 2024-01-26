4f92871c1353875b677fdb656ea70a88e8366870
Christian <c.ship.johnson@gmail.com>
2024-01-25 19:26:32 -0500

    Autocommit - 25Jan2024 19:26:29

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import utils
 from utils import *
 
+
+def populate_table(name=None, box=None, track=None):
+    table.delete(*table.get_children())
+    query="SELECT * FROM packages WHERE 1=1"
+    if name:
+        query+=f" AND adressee LIKE '%{name}%'"
+    if track:
+        query+=f" AND tracking_number LIKE '%{track}%'"
+    if box:
+        query+=f" AND box_number LIKE '%{box}%'"
+    results=cursor.execute(query)
+    results=cursor.fetchall()
+    for data in results:
+        table.insert('', 'end', values=(data[1], data[2], data[3], data[4]))
+
+
+
+
 root=tk.Tk()
 root.geomtry=("400x450")
 root.title("Mail Database")
@@ -26,9 +44,37 @@ package_button = ttk.Button(tab1, text="Enter Package", command=lambda: add_pack
 
 
 # Tab 2 - Package Retrieval
+
+search_name=ttk.Entry(tab2)
+search_name.grid(column=1, row=1, padx=10, pady=10)
+search_name_label=ttk.Label(tab2, text='Cadet Name').grid(row=1, column=0, padx=10, pady=10)
 search_box=ttk.Entry(tab2)
-search_box.grid(column=1, row=0, padx=10, pady=10)
-search_box_label=ttk.Label(tab2, text='Enter Cadet Name').grid(row=0, column=0, padx=10, pady=10)
+search_box.grid(column=1, row=2, padx=10, pady=10)
+search_box_label=ttk.Label(tab2, text='Box Number').grid(column=0, row=2, padx=10, pady=10)
+search_track=ttk.Entry(tab2)
+search_track.grid(column=1, row=3, padx=10, pady=10)
+search_track_label=ttk.Label(tab2, text='Tracking Number').grid(column=0, row=3, padx=10, pady=10)
+
+search_button=ttk.Button(tab2, text="Search", command=lambda: populate_table(name=search_name.get(), box=search_box.get(), track=search_track.get()))
+search_button.grid(row=4, column=0, columnspan=2)
+CreateToolTip(search_button, text='Fill out fields to search for a package.\n'
+                                  'The system will narrow results to match all fields;\n'
+                                  'leave fields blank to exclude them from the search')
+
+table=ttk.Treeview(tab2, columns=('track', 'name', 'received', 'picked'), show = 'headings')
+table.heading('track', text='Tracking Number')
+table.heading('name', text='Cadet Name')
+table.heading('received', text='Received')
+table.heading('picked', text='Picked Up')
+table.grid(column=3, row=1, padx=20, pady=10, columnspan=1, rowspan=4)
+
+# Events and Items.
+def item_select(_):
+    #print(table.selection())
+    for i in table.selection():
+        print(table.item(i)['values'])
+
+table.bind('<<TreeviewSelect>>', item_select)
 
 # Display Window
 root.mainloop()
--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import smtplib
 from email.mime.text import MIMEText
 import tkinter as tk
 from tkinter import ttk
+from tkinter import Toplevel
 from datetime import datetime
 
 
@@ -25,6 +26,39 @@ def add_package(box, track):
     cursor.execute("insert into packages(tracking_number,adressee,received) values ({track},'{name}','{date}')".format(track=track, name=name, date=date))
     connection.commit()
 
+class ToolTip(object):
+    def __init__(self,widget):
+        self.widget=widget
+        self.tipwindow=None
+        self.id=None
+        self.x=self.y=0
+    def showtip(self,text):
+        "Display text in tooltip window"
+        self.text=text
+        if self.tipwindow or not self.text:
+            return
+        x,y,cx,cy=self.widget.bbox("insert")
+        x=x+self.widget.winfo_rootx()+57
+        y=y+cy+self.widget.winfo_rooty()+27
+        self.tipwindow=tw=Toplevel(self.widget)
+        tw.wm_overrideredirect(1)
+        tw.wm_geometry("+%d+%d" % (x,y))
+        label=ttk.Label(tw,text=self.text,justify='left',relief='solid',borderwidth=1)
+        label.pack(ipadx=1)
+    def hidetip(self):
+        tw=self.tipwindow
+        self.tipwindow=None
+        if tw:
+            tw.destroy()
+
+def CreateToolTip(widget, text):
+    tooltip=ToolTip(widget)
+    def enter(event):
+        tooltip.showtip(text)
+    def leave(event):
+        tooltip.hidetip()
+    widget.bind('<Enter>', enter)
+    widget.bind('<Leave>', leave)
 
 def find_in_db(var):
     cursor.execute("SELECT * FROM packages WHERE adressee LIKE (?)", ('%'+var+'%',))
