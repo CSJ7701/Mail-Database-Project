57a4782dce5a17f67726c645ac6cb20dc12f2eb3
Christian <c.ship.johnson@gmail.com>
2024-04-08 15:01:29 -0400

    Autocommit - 08Apr2024 15:01:02

--- gui.py
+++ gui.py
@@ -9,7 +9,11 @@ from database import Database
 from login import User
 from datetime import datetime
 from config import Config
+import CTkListbox
 from PIL import Image
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+
 
 class GUI:
     def __init__(self, root, database, user):
@@ -787,9 +791,12 @@ class Reports(NavGUI):
         self.report_options_frame.pack(side="top", padx=10, pady=(0,10), fill="both", expand=True)
 
         # Choose Report Frame
-        self.report_choice_label=ctk.CTkLabel(self.choose_report_frame, text="Choose Report")
+
+        reports=["None", "All Time", "Day of the Week", "Time of Day", "Company", "Class"]
+        
+        self.report_choice_label=ctk.CTkLabel(self.choose_report_frame, text="Visualise packages by:")
         self.report_choice_label.pack(side="top", padx=10, pady=(20,0))
-        self.report_choice=ctk.CTkOptionMenu(self.choose_report_frame)
+        self.report_choice=ctk.CTkOptionMenu(self.choose_report_frame, values=reports)
         self.report_choice.pack(side="top", padx=10, pady=10)
 
         self.report_start_date_label=ctk.CTkLabel(self.choose_report_frame, text="Start Date")
@@ -802,6 +809,42 @@ class Reports(NavGUI):
         self.report_end_date_entry.pack(side="top", padx=10, pady=5)
 
         self.report_start_date_entry.bind("<FocusOut>", self.CheckDate)
+        self.report_end_date_entry.bind("<FocusOut>", self.CheckDate)
+
+        # Report Filter Frame
+
+        filters=["None", "Cadet Class", "Cadet Company"]
+        self.filter_choice_label=ctk.CTkLabel(self.report_options_frame, text="Filter report by:")
+        self.filter_choice=ctk.CTkOptionMenu(self.report_options_frame, values=filters)
+        self.filter_choice_label.pack(side="top", padx=10, pady=(10,0))
+        self.filter_choice.pack(side="top", padx=10, pady=(10,0))
+
+        companies=["Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel"]
+        years=self.GetYears()
+
+        self.exclude_label=ctk.CTkLabel(self.report_options_frame, text="Exclude:")
+        self.exclude_company=CTkListbox.CTkListbox(self.report_options_frame, multiple_selection=True, text_color="#000")
+        self.exclude_year=CTkListbox.CTkListbox(self.report_options_frame, multiple_selection=True, text_color="#000")
+        self.exclude_label.pack(side="top", padx=10, pady=(30,0))
+        self.exclude_company.pack(side="top", padx=10, pady=(10,0))
+        self.exclude_year.pack(side="top", padx=10, pady=(10,0))
+
+        for i in range(len(companies)):
+            if i==len(companies)-1:
+                ind="END"
+            else:
+                ind=i
+            self.exclude_company.insert(ind, companies[i])
+
+        for i in range(len(years)):
+            if i==len(years)-1:
+                ind="END"
+            else:
+                ind=i
+            self.exclude_year.insert(ind, years[i])
+
+        self.plot=ctk.CTkButton(self.report_options_frame, text="Plot")
+        self.plot.pack(side="bottom",padx=10, pady=20)
 
         # Average number of packages per day
         # # Sort by company, class
@@ -838,9 +881,18 @@ class Reports(NavGUI):
             except ValueError:
                 self.show_error("End date is incorrect.\nPlease format as 'YYYY-MM-DD'")
 
-
-
-
+    def GetYears(self):
+        CurrentYear=int(datetime.today().strftime("%Y"))
+        years=[]
+        i=5
+        while i>0:
+            years.append(CurrentYear-i)
+            i=i-1
+        i=0
+        while i<6:
+            years.append(CurrentYear+i)
+            i=i+1
+        return years
 
 class Settings(NavGUI):
     def __init__(self, main_frame, ParentGUI):
